458db26582541344c7bb12c470f15b057f60c478
Jack Waghan <jackwaghan@gmail.com>
2025-07-24 05:19:22 +0530

    feat: set runtime to edge for onboarding page

--- app/app/onboarding/page.tsx
+++ app/app/onboarding/page.tsx
@@ -2,6 +2,8 @@ import { cookies } from 'next/headers';
 import Progress from './progress';
 import { redirect } from 'next/navigation';
 
+export const runtime = 'edge';
+
 const Page = async () => {
   const cookieStore = await cookies();
   const onboarding = cookieStore.get('onboarding')?.value;
